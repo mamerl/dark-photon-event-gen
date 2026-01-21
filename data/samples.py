b108dac11b34aac5d44d65f679b49f61732edad3
mamerl <maximilian.amerl@cern.ch>
2026-01-21 23:55:06 +0100

    Update DMsimp metadata

--- data/samples.py
+++ data/samples.py
@@ -9,8 +9,20 @@ to load for analysis and to locate metadata.
 """
 
 samples = {
-    "DMsimp_mmed_600": {
-        "ntuple": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/run/dmsimp_mmed_600/Events/run_01/tag_1_delphes_events.root",
+    "DMsimp_mmed350": {
+        "ntuple": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/outputs/generated_events_dmsimp_mmed350_1.root",
+        "metadata": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/data/dmsimp_metadata.json",
+    },
+    "DMsimp_mmed600": {
+        "ntuple": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/outputs/generated_events_dmsimp_mmed600_1.root",
+        "metadata": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/data/dmsimp_metadata.json",
+    },
+    "DMsimp_mmed1000": {
+        "ntuple": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/outputs/generated_events_dmsimp_mmed1000_1.root",
+        "metadata": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/data/dmsimp_metadata.json",
+    },
+    "DMsimp_mmed2000": {
+        "ntuple": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/outputs/generated_events_dmsimp_mmed2000_1.root",
         "metadata": "/eos/user/m/mamerl/PhD/TLA/DijetISR/Interpretations/DMWG-dark-photon-tools/dark-photon-event-gen/data/dmsimp_metadata.json",
     },
 }
