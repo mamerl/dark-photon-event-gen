4e3c5ed319c7c9e8c0d586135d2d6badd0897aed
mamerl <maximilian.amerl@cern.ch>
2026-02-02 23:00:57 +0100

    Fix bugs in generation script for pythia

--- modules/pythia_generate.cxx
+++ modules/pythia_generate.cxx
@@ -2,6 +2,7 @@
 #include "Pythia8Plugins/HepMC2.h" // Use HepMC2
 #include <fstream>
 #include <string>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -9,39 +10,47 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    const std::string settingsFile = argv[1];
+    const std::string hepmcFile    = argv[2];
+    const std::string xsecFile     = argv[3];
+
     Pythia8::Pythia pythia;
     // Read settings from file
     // The first argument is the name of the input settings file
-    pythia.readFile(argv[1]); 
+    pythia.readFile(settingsFile); 
     pythia.init();
 
-    // Interface to write HepMC file
-    // The second argument is the name of the output HepMC file
-    Pythia8::Pythia8ToHepMC tophepmc(argv[2]);
+    // HepMC2 interface and ASCII output
+    HepMC::Pythia8ToHepMC toHepMC;
+    HepMC::IO_GenEvent ascii_io(hepmcFile, std::ios::out);
 
     int nEvent = pythia.mode("Main:numberOfEvents");
     for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
         if (!pythia.next()) continue;
         
-        // Convert and write the event
-        tophepmc.fillNextEvent(pythia);
+        // Create a HepMC event, fill it from Pythia and write it out
+        HepMC::GenEvent* hepmcEvt = new HepMC::GenEvent();
+        toHepMC.fill_next_event(pythia, hepmcEvt);
+        ascii_io << hepmcEvt;
+        delete hepmcEvt;
     }
 
-    // write cross section (mb) and its error to "<output_hepmc>.xsec"
-    std::string xsecFile = argv[3];
+    // Write cross section summary
     std::ofstream ofs(xsecFile);
     if (!ofs) {
-        std::cerr << "Error: cannot open cross-section file " << xsecFile << '\n';
+        std::cerr << "Error: cannot open cross-section file " << xsecFile << "\n";
     } else {
-        double sigma_mb = pythia.info.sigmaGen();
+        double sigma_mb     = pythia.info.sigmaGen();
         double sigma_err_mb = pythia.info.sigmaErr();
-        int nSelected = pythia.info.nSelected();
-        int nTried = pythia.info.nTried();
-        int nAccepted = pythia.info.nAccepted();
-        double weight_sum = pythia.info.weightSum();
+        int    nSelected    = pythia.info.nSelected();
+        int    nTried       = pythia.info.nTried();
+        int    nAccepted    = pythia.info.nAccepted();
+        double weight_sum   = pythia.info.weightSum();
 
         ofs << "# sigma (mb) sigmaErr (mb) nTried nSelected nAccepted sumW\n"
-            << sigma_mb << ' ' << sigma_err_mb << ' ' << nTried << ' ' << nSelected << ' ' << nAccepted << ' ' << weight_sum << '\n';
+            << sigma_mb << ' ' << sigma_err_mb << ' '
+            << nTried   << ' ' << nSelected    << ' '
+            << nAccepted << ' ' << weight_sum << "\n";
     }
 
     pythia.stat();
