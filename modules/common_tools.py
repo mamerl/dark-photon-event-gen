4ef638c4d75b0e29364f9d639967f4fab4de9b3f
mamerl <maximilian.amerl@cern.ch>
2026-02-11 16:41:43 +0100

    Add BR from HEPData to excited quark MC metadata and add BR to weighting for producing histograms

--- modules/common_tools.py
+++ modules/common_tools.py
@@ -36,10 +36,15 @@ def load_delhes_rdf(sample_id:str, file_path:str, metadata_path:str, tree_name="
     with open(metadata_path, 'r') as f:
         metadata = json.load(f)
 
+    # calculate the weight factor for normalising the event weights
+    # include the BR where this is defined in the metadata file, otherwise assume it is 1
+    # (i.e. the cross-section already includes the BR)
+    weight_factor = (metadata[sample_id]['xsec'] * metadata[sample_id].get('filter_eff', 1.0) * metadata[sample_id].get('br', 1.0)) / metadata[sample_id]['sumW']
+    
     # define a new column with normalised event weights
     rdf = rdf.Define(
         "mcEventWeight",
-        f"return {metadata[sample_id]['xsec']} * {metadata[sample_id].get('filter_eff', 1.0)} * (Event.Weight / {metadata[sample_id]['sumW']});"
+        f"return {weight_factor} * Event.Weight;"
     )
 
     return rdf
