e662c3b6b8b7797842d45d28a1449ef9db856c9f
mamerl <maximilian.amerl@cern.ch>
2026-03-02 17:05:13 +0100

    Add xsec limits to Z' exclusion comparisons

--- plotting/plot_dmsimp_exclusion.py
+++ plotting/plot_dmsimp_exclusion.py
@@ -32,6 +32,10 @@ run2_limits = {
 for sr in run2_limits:
     run2_limits[sr]["signal_strengths"] = (np.array(run2_limits[sr]["limit"]) / GQ_REFERENCE) ** 2
 
+run2_xsec_limits = dict()
+with open("analyses/run2_atlas_tla_dijet_xsec_limit.json", "r") as f:
+    run2_xsec_limits = json.loads(f.read())
+
 signal_regions = ["J50", "J100"]
 sample_list = [
     f"DMsimp_mmed{mass}" 
@@ -46,7 +50,7 @@ sample_list = [
 os.system(f"python modules/process_sample.py -s {' '.join(sample_list)} -o outputs/ -w 4 -r -t default -a run2_atlas_tla_dijet")
 
 limit_curve = {
-    sr: {"masses": [], "limits": [], "signal_strengths": []}
+    sr: {"masses": [], "limits": [], "signal_strengths": [], "xsec_limits": []}
     for sr in signal_regions
 }
 
@@ -72,10 +76,10 @@ for sample in sample_list:
             continue
         
         if np.isnan(excluded_xsec) or np.isnan(theory_expected_xsec):
-            logger.warning(f"nan value for excluded_xsec or theory_expected_xsec for sample {sample} and signal region {signal_region}, skipping")
+            logger.debug(f"nan value for excluded_xsec or theory_expected_xsec for sample {sample} and signal region {signal_region}, skipping")
             continue
         elif theory_expected_xsec == 0:
-            logger.warning(f"theory expected xsec is zero for sample {sample} and signal region {signal_region}, skipping")
+            logger.debug(f"theory expected xsec is zero for sample {sample} and signal region {signal_region}, skipping")
             continue
 
         limit_curve[signal_region]["masses"].append(samples[sample]["mass"])
@@ -86,6 +90,7 @@ for sample in sample_list:
         limit_curve[signal_region]["signal_strengths"].append(
             excluded_xsec / (theory_expected_xsec * 1.25)
         )
+        limit_curve[signal_region]["xsec_limits"].append(excluded_xsec)
 
 ################################################################
 ### Coupling limit plot 
@@ -172,7 +177,7 @@ ax[0].legend(
 )
 ax[0].text(
     0.03, 0.97,
-    r"$\sqrt{s} = 13$ TeV, 139 fb$^{-1}$" + "\n" + r"$Z' \rightarrow q\bar{q}$, $g_\chi = 1$, $m_\chi = 10$ TeV",
+    r"$\sqrt{s} = 13$ TeV, 15-132 fb$^{-1}$" + "\n" + r"$Z' \rightarrow q\bar{q}$, $g_\chi = 1$, $m_\chi = 10$ TeV",
     transform=ax[0].transAxes,
     fontsize=28,
     va="top",
@@ -187,7 +192,100 @@ plt.savefig("outputs/dmsimp_gq_exclusion_limits.pdf")
 ################################################################
 ### Cross-section limit plot
 
-# TODO pending limits 
+fig, ax = plt.subplots(2,1, figsize=(10,10), sharex=True, height_ratios=[3,1])
+plt.subplots_adjust(hspace=0.05)
+ax[1].set_xlabel(r"$m_{Z'}$ [GeV]", fontsize=28)
+ax[0].set_ylabel(r"$\sigma \times A \times BR$", fontsize=28)
+ax[1].set_ylabel("Ratio", fontsize=28)
+
+handles = []
+for signal_region, color in zip(signal_regions, ["C0", "C1"]):
+    # for each signal region sort the limit curve by mass
+    masses = np.array(limit_curve[signal_region]["masses"])
+    limits = np.array(limit_curve[signal_region]["xsec_limits"])
+    sorted_indices = np.argsort(masses)
+    masses = masses[sorted_indices]
+    limits = limits[sorted_indices]
+    # mask out any nan values
+    mask = ~np.isnan(limits)
+    masses = masses[mask]
+    limits = limits[mask]
+    ax[0].plot(
+        masses,
+        limits,
+        marker="s",
+        ls="--",
+        markersize=10,
+        fillstyle="none",
+        color=color,
+        lw=3,
+    )
+
+    ax[0].plot(
+        run2_xsec_limits[signal_region]["mass"],
+        run2_xsec_limits[signal_region]["limit"],
+        ls="-",
+        color=color,
+        lw=3,
+        marker="o",
+        markersize=10,
+    )
+
+    # calculate the difference between the published and reinterpretation limits
+    # at each mass point in the reinterpretation
+    run2_masses_mask = np.full_like(run2_xsec_limits[signal_region]["mass"], False, dtype=bool)
+    for mass in masses:
+        run2_masses_mask |= np.array(run2_xsec_limits[signal_region]["mass"]) == mass
+    ratio = limits / np.array(run2_xsec_limits[signal_region]["limit"])[run2_masses_mask]
+    ratio[np.isinf(ratio)] = np.nan
+
+    ax[1].plot(
+        masses,
+        ratio,
+        ls="--",
+        color=color,
+        marker="s",
+        markersize=10,
+        fillstyle="none",
+        lw=3,
+    )
+
+    handles.append(
+        plt.Line2D([], [], color=color, ls="-", lw=10, label=signal_region),
+    )
+
+handles += [
+    plt.Line2D([], [], color="black", ls="-", lw=5, marker="o", markersize=15, label="Published"),
+    plt.Line2D([], [], color="black", ls="--", lw=5, marker="s", fillstyle="none", markersize=15, label="Reinterpretation")
+]
+ax[1].set_ylim(0.0, 4)
+ax[0].set_ylim(1e-2,1200)
+ax[0].set_yscale("log")
+ax[1].axhline(1, color="black", ls=":", lw=2)
+ax[1].set_xlim(325, 1850)
+ax[1].set_xticks([400, 600, 800, 1000, 1200, 1400, 1600, 1800])
+ax[0].legend(
+    handles=handles,
+    handlelength=2.5, 
+    loc="lower center", 
+    fontsize=28, 
+    labelspacing=0.5, 
+    ncols=2,
+    bbox_to_anchor=(0.5, 0.5)
+)
+ax[0].text(
+    0.03, 0.97,
+    r"$\sqrt{s} = 13$ TeV, 15-132 fb$^{-1}$" + "\n" + r"$Z' \rightarrow q\bar{q}$, $g_\chi = 1$, $m_\chi = 10$ TeV",
+    transform=ax[0].transAxes,
+    fontsize=28,
+    va="top",
+    ha="left",
+)
+for a in ax:
+    a.tick_params(axis="both", which="major", labelsize=28, pad=10)
+
+logger.info("saving signal strength plot to outputs/dmsimp_xsec_exclusion_limits.pdf")
+plt.savefig("outputs/dmsimp_xsec_exclusion_limits.pdf")
 
 ################################################################
 ### Signal strength plot
@@ -274,7 +372,7 @@ ax[0].legend(
 )
 ax[0].text(
     0.03, 0.97,
-    r"$\sqrt{s} = 13$ TeV, 139 fb$^{-1}$" + "\n" + r"$Z' \rightarrow q\bar{q}$, $g_\chi = 1$, $m_\chi = 10$ TeV",
+    r"$\sqrt{s} = 13$ TeV, 15-132 fb$^{-1}$" + "\n" + r"$Z' \rightarrow q\bar{q}$, $g_\chi = 1$, $m_\chi = 10$ TeV",
     transform=ax[0].transAxes,
     fontsize=28,
     va="top",
